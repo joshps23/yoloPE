be490dc7879f3a039a73dc2af77f7b5dde10b6a1
joshps23 <huixmun@gmail.com>
2024-07-31 06:30:54 +0800

    added heatmap

--- YOLO_Video.py
+++ YOLO_Video.py
@@ -1,4 +1,5 @@
-from ultralytics import YOLO
+from ultralytics import YOLO, solutions
+
 import cv2
 import math
 
@@ -6,12 +7,33 @@ def video_detection(path_x):
     video_capture = path_x
     #Create a Webcam Object
     cap=cv2.VideoCapture(video_capture)
+    # heatmap_obj = heatmap.Heatmap()
     frame_width=int(cap.get(3))
     frame_height=int(cap.get(4))
     #out=cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc('M', 'J', 'P','G'), 10, (frame_width, frame_height))
-
-    model=YOLO("ballDetectBestV2.pt")
-    classNames = ['ball']
+    classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+                "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+                "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+                "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+                "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+                "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+                "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+                "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+                "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+                "teddy bear", "hair drier", "toothbrush"
+                ]
+    
+    model=YOLO("yolov8n.pt")
+    # classNames = ['ball']
+    success, img = cap.read()
+    h,w,c = img.shape
+    heatmap_obj=solutions.Heatmap(colormap=cv2.COLORMAP_PARULA,
+    imw=int(w),
+    imh=int(h),
+    view_img=False,
+    shape="circle",
+    classes_names=model.names
+    )
     while True:
         success, img = cap.read()
         results=model(img,stream=True)
@@ -40,8 +62,9 @@ def video_detection(path_x):
                     cv2.rectangle(img, (x1,y1), (x2,y2), color,3)
                     cv2.rectangle(img, (x1,y1), c2, color, -1, cv2.LINE_AA)  # filled
                     cv2.putText(img, label, (x1,y1-2),0, 1,[255,255,255], thickness=1,lineType=cv2.LINE_AA)
-
-        yield img
+        track=model.track(img)
+        final_img=heatmap_obj.generate_heatmap(img,track)
+        yield final_img
         #out.write(img)
         #cv2.imshow("image", img)
         #if cv2.waitKey(1) & 0xFF==ord('1'):
