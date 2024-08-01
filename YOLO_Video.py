2fb280cb813844315c4ff3426ef98e7efa7b6d5e
joshps23 <huixmun@gmail.com>
2024-08-01 09:31:00 +0800

    adjusted dimensions

--- YOLO_Video.py
+++ YOLO_Video.py
@@ -46,22 +46,22 @@ def video_detection(path_x):
                 conf=math.ceil((box.conf[0]*100))/100
                 cls=int(box.cls[0])
                 class_name=classNames[cls]
-                label=f'{class_name}{conf}'
-                t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
-                print(t_size)
-                c2 = x1 + t_size[0], y1 - t_size[1] - 3
-                if class_name == 'Dust Mask':
-                    color=(0, 204, 255)
-                elif class_name == "Glove":
-                    color = (222, 82, 175)
-                elif class_name == "Protective Helmet":
-                    color = (0, 149, 255)
-                else:
-                    color = (85,45,255)
-                if conf>0.5:
-                    cv2.rectangle(img, (x1,y1), (x2,y2), color,3)
-                    cv2.rectangle(img, (x1,y1), c2, color, -1, cv2.LINE_AA)  # filled
-                    cv2.putText(img, label, (x1,y1-2),0, 1,[255,255,255], thickness=1,lineType=cv2.LINE_AA)
+                # label=f'{class_name}{conf}'
+                # t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
+                # print(t_size)
+                # c2 = x1 + t_size[0], y1 - t_size[1] - 3
+                # if class_name == 'Dust Mask':
+                #     color=(0, 204, 255)
+                # elif class_name == "Glove":
+                #     color = (222, 82, 175)
+                # elif class_name == "Protective Helmet":
+                #     color = (0, 149, 255)
+                # else:
+                #     color = (85,45,255)
+                # if conf>0.5:
+                #     cv2.rectangle(img, (x1,y1), (x2,y2), color,3)
+                #     cv2.rectangle(img, (x1,y1), c2, color, -1, cv2.LINE_AA)  # filled
+                #     cv2.putText(img, label, (x1,y1-2),0, 1,[255,255,255], thickness=1,lineType=cv2.LINE_AA)
         track=model.track(img)
         final_img=heatmap_obj.generate_heatmap(img,track)
         yield final_img
