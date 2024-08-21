fcdcb66905f9e663deb2d1d0ab6ef158dacf8ff1
joshps23 <huixmun@gmail.com>
2024-08-22 01:09:52 +0800

    font color green

--- YOLO_Video_Class.py
+++ YOLO_Video_Class.py
@@ -30,6 +30,6 @@ def video_classify(path_x):
       text_size, _ = cv2.getTextSize(label, font, font_scale, font_thickness)
       text_w, text_h = text_size
       cv2.rectangle(img,((frame_width//2)-(text_w//2),(frame_height//2)-text_h),((frame_width//2)+(text_w//2),(frame_height//2)),text_color_bg,-1)
-      cv2.putText(img, label, ((frame_width//2)-(text_w//2),(frame_height//2)),cv2.FONT_HERSHEY_SIMPLEX, 20,[255,255,255], thickness=20,lineType=cv2.LINE_AA)
+      cv2.putText(img, label, ((frame_width//2)-(text_w//2),(frame_height//2)),cv2.FONT_HERSHEY_SIMPLEX, 20,[0,255,0], thickness=20,lineType=cv2.LINE_AA)
 
       yield img
